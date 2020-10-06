9d607174814c83c6c5e8e24b95f30d10249b1379
Raphaëlle Jaillet <raphaelle.jaillet@yahoo.fr>
2020-10-06 12:46:49 +0200

    suite methode TDD en refactorisant

--- RomanNumeralsConsole/RomanNumerals.cs
+++ RomanNumeralsConsole/RomanNumerals.cs
@@ -8,14 +8,31 @@ namespace RomanNumeralsConsole
     {
         public static int Translate (string romanNumber)
         {
-            if (romanNumber == "I")
-                return 1;
-            else if (romanNumber == "V")
-                return 5;
-            else if (romanNumber == "X")
-                return 10;
+            if (romanNumber.Length == 1)
+            {
+                if (romanNumber == "I")
+                    return 1;
+                else if (romanNumber == "V")
+                    return 5;
+                else if (romanNumber == "X")
+                    return 10;
+            }
             else
-                return 0;
+            {
+                int result = 0;
+                int iDigit = 0;
+                while(iDigit < romanNumber.Length - 1 && romanNumber[iDigit] == romanNumber[iDigit + 1])
+                {
+                    result++;
+                    iDigit++;
+                }
+                if (result > 0)
+                    return result + 1;
+                else if (romanNumber == "IV")
+                    return 4;
+            }
+
+            return 0;
         }
 
     }
--- RomanNumeralsTest/TranslateShould.cs
+++ RomanNumeralsTest/TranslateShould.cs
@@ -23,5 +23,23 @@ namespace RomanNumeralsTest
         {
             Assert.Equal(10, RomanNumerals.Translate("X"));
         }
+
+        [Fact]
+        public void ReturnIIwhen2()
+        {
+            Assert.Equal(2, RomanNumerals.Translate("II"));
+        }
+
+        [Fact]
+        public void ReturnIIIwhen3()
+        {
+            Assert.Equal(3, RomanNumerals.Translate("III"));
+        }
+
+        [Fact]
+        public void ReturnIVwhen4()
+        {
+            Assert.Equal(4, RomanNumerals.Translate("IV"));
+        }
     }
 }
