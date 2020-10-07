c7871a0bf286305af21684efe902b496de3dc185
Raphaëlle Jaillet <raphaelle.jaillet@yahoo.fr>
2020-10-07 10:39:17 +0200

    rajout customer

--- DecouverteEntityFrameworkQuery/Program.cs
+++ DecouverteEntityFrameworkQuery/Program.cs
@@ -11,10 +11,14 @@ namespace DecouverteEntityFrameworkQuery
         {
             var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
             optionsBuilder.UseSqlServer(@"Server=51.178.46.82,1533;Initial Catalog=FormationCSharpSQL;Persist Security Info=False;User ID=FormationCSharpSQL;Password=FormationCSharp&SQL;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;");
+            //optionsBuilder.UseSqlServer(@"Server=.\sqlexpress;Database=DecouverteEntityFrameworkCodeFirst;Trusted_Connection=True;");
             MyDbContext myDbContext = new MyDbContext(optionsBuilder.Options);
 
             var items = (from i in myDbContext.Items
                          select i).ToList();
+
+            var customers = (from i in myDbContext.Customers
+                             select i).ToList();
 
         }
     }
