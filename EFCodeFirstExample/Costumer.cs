029d3da1c82b158a9970e47e1eba991e8081de04
newtomsoft <thomas@newtomsoft.fr>
2020-10-07 13:00:57 +0200

    correction projet EFCodeFirstExample

---
+++ EFCodeFirstExample/Costumer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCodeFirstExample
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; }
+
+        [MaxLength(30)]
+        public string City { get; set; }
+
+        public DateTime? BirthDay { get; set; }
+    }
+}
---
+++ EFCodeFirstExample/Migrations/20201007105752_ExempleCreationDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCodeFirstExample.Migrations
+{
+    public partial class ExempleCreationDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 30, nullable: false),
+                    Email = table.Column<string>(maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<string>(maxLength: 10, nullable: true),
+                    City = table.Column<string>(maxLength: 30, nullable: true),
+                    BirthDay = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
