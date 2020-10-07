88a265fec5d3069abb7751d63f9cb758ad34e576
Emilie <emilie.macary@gmail.com>
2020-10-07 13:20:11 +0200

    requete sql

--- DecouverteEntityFrameworkQuery/Program.cs
+++ DecouverteEntityFrameworkQuery/Program.cs
@@ -21,14 +21,35 @@ namespace DecouverteEntityFrameworkQuery
                              select c).ToList();
 
             var customersOrders = (from customer in myDbContext.Customers
-                          join order in myDbContext.Orders on customer.Id equals order.CustomerId
-                          select new { customer, order }).ToList();
+                                   join order in myDbContext.Orders on customer.Id equals order.CustomerId
+                                   select new { customer, order }).ToList();
 
-            foreach(var co in customersOrders)
+            foreach (var co in customersOrders)
             {
                 Console.WriteLine($"Le client {co.customer.FirstName} {co.customer.Name} a fait la commande numéro {co.order.Id} à la date du {co.order.Date}");
             }
 
+            var ReferenceDate = new DateTime(2020, 02, 01);
+            var customersDateOrders = (from customer in myDbContext.Customers
+                                       join order in myDbContext.Orders on customer.Id equals order.CustomerId
+                                       where order.Date > ReferenceDate
+                                       select new { customer, order }).ToList();
+
+            Console.WriteLine($"Commande passée après le {ReferenceDate}");
+            foreach (var co in customersDateOrders)
+            {
+                Console.WriteLine($"Le client {co.customer.FirstName} {co.customer.Name} a fait la commande numéro {co.order.Id} à la date du {co.order.Date}");
+            }
+
+            var customersDateOrders2 = from co in customersOrders
+                                       where co.order.Date > ReferenceDate
+                                       select co;
+
+            Console.WriteLine($"Nouvelle methode : Commande passée après le {ReferenceDate}");
+            foreach (var coDate in customersDateOrders2)
+            {
+                Console.WriteLine($"Le client {coDate.customer.FirstName} {coDate.customer.Name} a fait la commande numéro {coDate.order.Id} à la date du {coDate.order.Date}");
+            }
 
         }
     }
