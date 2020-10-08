84c65e73f269ef4a3793df9217814ff5f71868db
Raphaëlle Jaillet <raphaelle.jaillet@yahoo.fr>
2020-10-08 16:10:38 +0200

    creation du design de la calculette

---
+++ Calculette/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculette
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_On_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_On_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_On_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_On_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_On_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_On_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_On_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_On_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
